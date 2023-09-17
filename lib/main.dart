372048c56224a3e85c2defbcfb3b5d27d30790c1
luanacolombo <luuana.colombo@gmail.com>
2023-09-17 20:56:57 -0300

    Criação do app e ajustes iniciais

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/pages/todo_list_page';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: //home é o parâmetro que especifica a tela inicial do app
+          TodoListPage(),
+    );
+  }
+}
